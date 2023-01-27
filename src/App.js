e046d60cfdd4d99af4aa1f79a7cd280bcf1b229c
AKSutar <sonuthesilentkiller@gmail.com>
2023-01-27 19:55:04 +0530

    reactset2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(arr1) {
+  const arr = arr1.concat().sort();
+  const a1 = arr[0];
+  const a2 = arr[arr.length - 1];
+  const L = a1.length;
+  let i = 0;
+  while (i < L && a1.charAt(i) === a2.charAt(i)) i++;
+  return a1.substring(0, i);
 }
+
+console.log((["go", "google"]));
+
+console.log((["SQLInjection", "SQLTutorial"]));
+
+console.log((["abcd", "1234"]));
 
 export default App;
